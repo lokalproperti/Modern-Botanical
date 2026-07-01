1686767ec699f9d0cee6e84fdd091781661ffb5a
agungset0641 <8703389@gmail.com>
2026-07-01 08:55:30 +0700

    compiled tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'botanical-bg': '#F4F7F4',
+        'botanical-text': '#1C2820',
+        'botanical-accent': '#4A5D52',
+      },
+      fontFamily: {
+        'cormorant': ['Cormorant Garamond', 'serif'],
+        'inter': ['Inter', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
